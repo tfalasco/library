e42905bd6d78f6d0c598ab1d528e50d5445cbcdc
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-01-15 05:56:09 -0500

    Add placeholder books for testing

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
+
+// Books added for testing
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 346, true);
+addBookToLibrary("The Starter Villain", "John Scalzi", 264, true);
+addBookToLibrary("The Three-Body Problem", "Liu Cixin", 448, true);
+addBookToLibrary("Team of Five", "Kate Anderson Brower", 320, false);
