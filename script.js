6c0d5a1769405316cf075d6fc1955d5058652dd1
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-01-15 05:56:55 -0500

    Add a function to display all books in the library

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// An array to store books
 const myLibrary = [];
 
+// The book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,6 +16,7 @@ function Book(title, author, pages, read) {
     }
 }
 
+// A function to create a book and add it to the library
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
@@ -23,3 +26,31 @@ addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 346, true);
 addBookToLibrary("The Starter Villain", "John Scalzi", 264, true);
 addBookToLibrary("The Three-Body Problem", "Liu Cixin", 448, true);
 addBookToLibrary("Team of Five", "Kate Anderson Brower", 320, false);
+
+// Display all books in library
+function displayAllBooks() {
+    var tableRef = document.getElementById("table");
+    myLibrary.forEach(book => {
+        // Insert a row
+        var newRow = tableRef.insertRow();
+
+        // Add a cell to the row for the title
+        var newCell = newRow.insertCell();
+        newCell.innerHTML = book.title;
+
+        // Add a cell for the author
+        newCell = newRow.insertCell();
+        newCell.innerHTML = book.author;
+        
+        // Add a cell for the number of pages
+        newCell = newRow.insertCell();
+        newCell.innerHTML = book.pages;
+
+        // Add a cell for the read status
+        newCell = newRow.insertCell();
+
+        newCell.innerHTML = book.read;
+    });
+}
+
+displayAllBooks();
